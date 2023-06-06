d37cf797e578ac9836078d231b4e4f88f0e676bc
Raymond Zou <raymond.1zou@gmail.com>
2023-06-06 11:46:36 -0700

    add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+  [SerializeField] Transform player;
+
+  // Update is called once per frame
+  void Update() {
+    transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+  }
+}
