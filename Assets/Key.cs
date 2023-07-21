64b51a27a1c3f854b2c40ca5cb8ecba6bc2bf38d
Raymond Zou <raymond.1zou@gmail.com>
2023-07-21 12:19:48 -0700

    add keys that update portal sprite

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour {
 
-  private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.tag == "Player") Destroy(gameObject);
-    Debug.Log("key has been collected");
-  }
+  public enum KeyColor { Orange, Purple, Green }
+  public static event Action<KeyColor> ColorChanged;
 
+  [SerializeField] private KeyColor color = KeyColor.Orange;
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (collision.tag == "Player") {
+      ColorChanged?.Invoke(color);
+      Destroy(gameObject);
+    }
+  }
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,8 +5,33 @@ using System;
 
 public class Portal : MonoBehaviour {
 
+  private bool isOpen = false;
+  [SerializeField] GameObject orangeKey;
+  [SerializeField] GameObject purpleKey;
+  [SerializeField] GameObject greenKey;
+
+  private void Awake() {
+    Key.ColorChanged += CollectKey;
+  }
+
+  private void CollectKey(Key.KeyColor color) {
+    Debug.Log(color + " key has been collected.");
+    //if (color == Key.KeyColor.Orange) orangeKey.SetActive(true);
+    switch (color) {
+      case Key.KeyColor.Orange:
+        orangeKey.SetActive(true);
+        break;
+      case Key.KeyColor.Green:
+        greenKey.SetActive(true);
+        break;
+      case Key.KeyColor.Purple:
+        purpleKey.SetActive(true);
+        break;
+    }
+  }
+
   private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.CompareTag("Player")) {
+    if (collision.CompareTag("Player") && isOpen) {
       GameManager.LoadNextLevel();
     }
   }
