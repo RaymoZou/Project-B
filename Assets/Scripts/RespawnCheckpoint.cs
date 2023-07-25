0228cd9f270803f74d600d34b28e4952af4c43a8
Raymond Zou <raymond.1zou@gmail.com>
2023-07-25 09:58:12 -0700

    add fire respawn animation

--- Assets/Scripts/RespawnCheckpoint.cs
+++ Assets/Scripts/RespawnCheckpoint.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 
 public class RespawnCheckpoint : MonoBehaviour {
 
-  [SerializeField] Sprite activatedCheckpoint;
-  SpriteRenderer spriteRenderer;
+  [SerializeField] SpriteRenderer activatedCheckpoint;
   public static event Action<Vector2> OnActivate; 
-
-  private void Awake() {
-    spriteRenderer = GetComponent<SpriteRenderer>();
-  }
 
   private void OnTriggerEnter2D(Collider2D collision) {
     if (collision.CompareTag("Player")) {
-      spriteRenderer.sprite = activatedCheckpoint;
+      activatedCheckpoint.gameObject.SetActive(true);
       OnActivate?.Invoke(new Vector3(transform.position.x, transform.position.y));
     }
   }
