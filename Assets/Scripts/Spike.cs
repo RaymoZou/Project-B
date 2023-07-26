8e7c745d3aa3c6b91652793bc93c57071a349b6f
Raymond Zou <raymond.1zou@gmail.com>
2023-07-26 10:18:16 -0700

    add persistent spike damage

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour {
 
+  [SerializeField] float damageTimer = 1f;
+  [SerializeField] int damage = 25;
+  public float currDamageTimer;
+
   private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.tag == "Player") {
-      Health playerHealth = collision.GetComponent<Health>();
-      playerHealth.DamagePlayer(25);
+    if (collision.tag != "Player") return;
+    Health playerHealth = collision.GetComponent<Health>();
+    doDamage(playerHealth, damage);
+    currDamageTimer = damageTimer;
+  }
+
+  private void OnTriggerStay2D(Collider2D collision) {
+
+    if (collision.tag != "Player") return;
+    Health playerHealth = collision.GetComponent<Health>();
+    currDamageTimer -= Time.deltaTime;
+    if (currDamageTimer < 0) {
+      doDamage(playerHealth, damage);
+      currDamageTimer = damageTimer;
     }
+  }
+
+  private void doDamage(Health health, int dmg) {
+    health.DamagePlayer(dmg);
   }
 }
