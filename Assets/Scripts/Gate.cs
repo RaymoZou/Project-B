a40454512b53242f98a075276f035d9456e29b29
Raymond Zou <raymond.1zou@gmail.com>
2023-08-04 15:36:31 -0700

    add empty block sprite

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gate : MonoBehaviour {
+  [SerializeField] Sprite emptySprite;
+  [SerializeField] Sprite solidSprite;
+  [SerializeField] bool isOpen;
+  private SpriteRenderer spriteRenderer;
+  private BoxCollider2D boxCollider;
 
   private Key.KeyColor colorCode = Key.KeyColor.Orange;
 
   private void Awake() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    boxCollider = GetComponent<BoxCollider2D>();
     Switch.OnInteract += HandleTriggered;
+  }
+
+  private void Start() {
+    spriteRenderer.sprite = isOpen ? emptySprite : solidSprite;
+  }
+
+  private void OnDestroy() {
+    Switch.OnInteract -= HandleTriggered;
   }
 
   private void HandleTriggered(Key.KeyColor color) {
     if (colorCode != color) return;
-    gameObject.SetActive(!isActiveAndEnabled);
+    isOpen = !isOpen;
+    if (isOpen) {
+      spriteRenderer.sprite = emptySprite;
+      boxCollider.enabled = false;
+    } else {
+      spriteRenderer.sprite = solidSprite;
+      boxCollider.enabled = true;
+    }
   }
 }
