ed65b20c9c1fc066f2072c4fd9a358994e75cbb0
RaymoZou <raymond.1zou@gmail.com>
2024-06-26 21:34:54 -0700

    change access levels of variables

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretScript : MonoBehaviour {
-  public GameObject Bullet;
+  [SerializeField] GameObject Bullet;
+  // TODO: make this a constant offset - remove need for a Transform that
+  // needs to be configured in the Inspector
   public Transform ShootPoint;
-  public float FireRate;
-  public float Force;
+  private float FIRE_RATE = 0.75f;
+  private float FORCE = 50;
   float nextTimeToFire = 0;
 
   // Update is called once per frame
   void Update() {
     if (Time.time > nextTimeToFire) {
-      nextTimeToFire = Time.time + 1 / FireRate;
+      nextTimeToFire = Time.time + 1 / FIRE_RATE;
       shoot();
     }
   }
 
   void shoot() {
     GameObject BulletIns = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
-    BulletIns.GetComponent<Rigidbody2D>().AddForce(Vector2.left * Force);
+    BulletIns.GetComponent<Rigidbody2D>().AddForce(Vector2.left * FORCE);
   }
 }
