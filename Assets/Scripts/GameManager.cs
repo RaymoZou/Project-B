5f9e233a0f3e715302c969ac80b133b22af8ec52
RaymoZou <raymond.1zou@gmail.com>
2024-06-29 07:55:48 -0700

    separate respawns based on player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,76 +4,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
   private static GameManager instance;
-  private Vector3 playerSpawn = Vector3.zero;
-
-  private int playerOneLayerIndex;
-  private int playerTwoLayerIndex;
+  private Vector3 playerOneSpawn = Vector2.zero;
+  private Vector3 playerTwoSpawn = Vector2.zero;
+  private int playerOneID; // the gameObject layer
+  private int playerTwoID; // the gameObject layer
   [SerializeField] private GameObject playerOnePrefab;
   [SerializeField] private GameObject playerTwoPrefab;
 
-  public void Awake()
-  {
-    playerOneLayerIndex = playerOnePrefab.layer;
-    playerTwoLayerIndex = playerTwoPrefab.layer;
+  public void Awake() {
     Health.OnDeath += RespawnPlayer;
     SceneManager.sceneLoaded += OnSceneLoaded;
     RespawnCheckpoint.OnActivate += UpdateSpawn;
+    playerOneID = LayerMask.NameToLayer("Player 1");
+    playerTwoID = LayerMask.NameToLayer("Player 2");
 
-    if (instance != null && instance != this)
-    {
+    // singleton pattern
+    if (instance != null && instance != this) {
       Destroy(gameObject);
       return;
     }
-    instance = this;
-    DontDestroyOnLoad(gameObject);
+    else {
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+    }
   }
 
-  private void OnDestroy()
-  {
+  private void OnDestroy() {
     Health.OnDeath -= RespawnPlayer;
     RespawnCheckpoint.OnActivate -= UpdateSpawn;
   }
 
-  public void UpdateSpawn(Vector2 spawnPos)
-  {
-    playerSpawn = spawnPos;
+  public void UpdateSpawn(Vector2 spawnPos, int playerLayer) {
+    if (playerLayer == playerOneID) playerOneSpawn = spawnPos;
+    if (playerLayer == playerTwoID) playerTwoSpawn = spawnPos;
   }
 
-  private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-  {
-    UpdateSpawn(Vector2.zero);
+  // initialize spawns to zero on level load
+  private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+    UpdateSpawn(Vector2.zero, playerOneID);
+    UpdateSpawn(Vector2.zero, playerTwoID);
   }
 
-  public static void LoadNextLevel()
-  {
+  // load the next level defined in build settings
+  public static void LoadNextLevel() {
     int currSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    if (currSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
-    {
+    if (currSceneIndex < SceneManager.sceneCountInBuildSettings - 1) {
       SceneManager.LoadScene(currSceneIndex + 1);
-    } else
-    {
+    }
+    else {
       SceneManager.LoadScene(0);
     }
   }
 
-  public static void RespawnPlayer(GameObject player)
-  {
+  // respawn the correct prefab of the player
+  public static void RespawnPlayer(GameObject player) {
     int playerLayer = player.layer;
     Destroy(player);
-    if (playerLayer == LayerMask.NameToLayer("Player 1"))
-    {
-      Instantiate(instance.playerOnePrefab, instance.playerSpawn, Quaternion.identity);
-    } else if (player.layer == LayerMask.NameToLayer("Player 2"))
-    {
-      Instantiate(instance.playerTwoPrefab, instance.playerSpawn, Quaternion.identity);
-    } else
-    {
+    if (playerLayer == LayerMask.NameToLayer("Player 1")) {
+      Instantiate(instance.playerOnePrefab, instance.playerOneSpawn, Quaternion.identity);
+    }
+    else if (player.layer == LayerMask.NameToLayer("Player 2")) {
+      Instantiate(instance.playerTwoPrefab, instance.playerTwoSpawn, Quaternion.identity);
+    }
+    else {
       Debug.LogError("Player Layer not known");
     }
 
+  }
+
+  public static void SwitchScreenResolution() {
+    Screen.SetResolution(1920, 1080, FullScreenMode.Windowed, 144);
+  }
+
+  // TODO: this seems really unnecessary
+  public static void QuitGame() {
+    Application.Quit();
   }
 }
--- Assets/Scripts/RespawnCheckpoint.cs
+++ Assets/Scripts/RespawnCheckpoint.cs
@@ -6,15 +6,13 @@ using UnityEngine;
 public class RespawnCheckpoint : MonoBehaviour {
 
   [SerializeField] SpriteRenderer activatedCheckpoint;
-  public static event Action<Vector2> OnActivate; 
+  public static event Action<Vector2, int> OnActivate;
 
   private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.CompareTag("Player")) {
-      if (collision.gameObject.layer == gameObject.layer)
-      {
-        activatedCheckpoint.gameObject.SetActive(true);
-        OnActivate?.Invoke(new Vector3(transform.position.x, transform.position.y));
-      }
+    if (!collision.CompareTag("Player")) return; // if it's not a player then return
+    if (collision.gameObject.layer == gameObject.layer) {
+      activatedCheckpoint.gameObject.SetActive(true);
+      OnActivate?.Invoke(new Vector2(transform.position.x, transform.position.y), gameObject.layer);
     }
   }
 
