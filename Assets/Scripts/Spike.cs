7c2aacc7ae26c6a8bd42b8d2f518080de0dfd29d
RaymoZou <raymond.1zou@gmail.com>
2024-07-01 08:03:30 -0700

    add spike prefab

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(Light2D))]
 
 public class Spike : MonoBehaviour {
 
   [SerializeField] int damage = 25;
+  new PolygonCollider2D collider;
+  private void Awake() {
+    collider = GetComponent<PolygonCollider2D>();
+    collider.isTrigger = true;
+  }
 
   private void OnTriggerEnter2D(Collider2D collision) {
     if (collision.tag != "Player") return;
