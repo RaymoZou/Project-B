ac5ccd2e13088f616f4c6f8180d11ec309ab3d4e
RaymoZou <raymond.1zou@gmail.com>
2024-06-30 22:34:18 -0700

    dynamic camera assignment

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,24 @@ public class CameraFollow : MonoBehaviour {
 
   private Transform playerTransform;
   readonly float followSpeed = 1.5f;
+  private static int numCameras = 0;
 
   private void Awake() {
-    // allocate screen space
-    if (gameObject.layer == LayerMask.NameToLayer("Player 1")) {
-      GetComponent<Camera>().rect = new Rect(0f, 0f, 0.5f, 1f);
-    } else if (gameObject.layer == LayerMask.NameToLayer("Player 2")) {
-      GetComponent<Camera>().rect = new Rect(0.5f, 0f, 0.5f, 1f);
-    }
+    numCameras++;
+    // allocate screen space for two cameras
     Health.OnSpawn += SetPlayerTransform; // reset the playerTransform when the player spawns
+  }
+
+  private void Start() {
+    if (numCameras == 1) {
+      GetComponent<Camera>().rect = new Rect(0f, 0f, 1f, 1f);
+    } else if (numCameras == 2) {
+      if (gameObject.layer == LayerMask.NameToLayer("Player 1")) {
+        GetComponent<Camera>().rect = new Rect(0f, 0f, 0.5f, 1f);
+      } else if (gameObject.layer == LayerMask.NameToLayer("Player 2")) {
+        GetComponent<Camera>().rect = new Rect(0.5f, 0f, 0.5f, 1f);
+      }
+    }
   }
 
   // set the player transform the camera will be following
