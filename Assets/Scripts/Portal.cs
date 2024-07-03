4aa7af6781aa07f962343cdc69572a03a92f8507
RaymoZou <raymond.1zou@gmail.com>
2024-07-02 23:12:42 -0700

    update Portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,47 +6,8 @@ using UnityEngine.Rendering.Universal;
 
 public class Portal : MonoBehaviour {
 
-  private bool isOpen = false;
-  [SerializeField] GameObject orangeKey;
-  [SerializeField] GameObject purpleKey;
-  [SerializeField] GameObject greenKey;
-  [SerializeField] Sprite openSprite;
-
-  [SerializeField] GameObject portalLight;
-
-  private SpriteRenderer spriteRenderer;
-
-  private void Awake() {
-    spriteRenderer = GetComponent<SpriteRenderer>();
-    Key.ColorChanged += CollectKey;
-  }
-
-  private void OnDestroy() {
-    Key.ColorChanged -= CollectKey;
-  }
-
-  private void CollectKey(Key.KeyColor color) {
-    switch (color) {
-      case Key.KeyColor.Orange:
-        orangeKey.SetActive(true);
-        break;
-      case Key.KeyColor.Green:
-        greenKey.SetActive(true);
-        break;
-      case Key.KeyColor.Purple:
-        purpleKey.SetActive(true);
-        break;
-    }
-
-    isOpen = orangeKey.activeSelf && greenKey.activeSelf && purpleKey.activeSelf;
-    if (isOpen) {
-      spriteRenderer.sprite = openSprite;
-      portalLight.SetActive(true);
-    }
-  }
-
   private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.CompareTag("Player") && isOpen) {
+    if (collision.CompareTag("Player")) {
       GameManager.LoadNextLevel();
     }
   }
