4b6dbf2fbc483aa92271816f7e771c7dfc1e849b
RaymoZou <raymond.1zou@gmail.com>
2024-07-04 20:15:00 -0700

    refactor + remove gravity modifier

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,124 +13,107 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
 
   Rigidbody2D rb;
-  Animator myAnimator;
+  Animator animator;
   SpriteRenderer spriteRenderer;
 
-  [Header("Ground check")]
-  private bool isGrounded = false;
   const float GROUND_CHECK_OFFSET = -0.50f; // relative to player position
   const float MIN_GROUND_DISTANCE = 0.1f;
 
-  [Header("Movement Settings")]
   const float TOP_SPEED = 4f;
   const float ACCEL_RATE = 7.5f;
   const float DEACCEL_RATE = 5f;
   const float JUMP_FORCE = 7f;
   const float MAX_JUMP_TIME = 0.35f;
-  const float GRAVITY_MULTIPLIER = 1.25f;
-  const float GRAVITY_SCALE = 1f;
   const float VEL_POWER = 1.25f;
   const float MAX_FALL_SPEED = 10.0f;
   const float WALL_JUMP_CD = 0.2f;
-  public float WALL_JUMP_X_FORCE = 16.0f; // TODO: change this const
-  public float WALL_JUMP_Y_FORCE = 8.0f;
-  private bool isJumping = false;
-  public bool isTouchingWall = false;
-  private float xInput;
-  private float currJumpTime;
-
-  [Header("Dash")]
+  const float WALL_JUMP_X_FORCE = 16.0f;
+  const float WALL_JUMP_Y_FORCE = 8.0f;
   const float DASH_FORCE = 5.0f;
   const float DASH_DURATION = 0.1f;
-  const float DASH_COOLDOWN = 2f;
-  private bool isDashing = false;
-  private float currDashCooldown;
-  public static event Action<float, int> OnDashChange;
-  private float currDashDuration;
-
-  [Header("Player Input")]
-  private PlayerInput playerInput;
-  private bool isJumpInput;
-  private bool isDashingInput;
-  private Vector2 currDirection;
+  const float DASH_CD = 2f;
   const float WALL_DETECTOR_LENGTH = 0.27f; // wall detection in front of the player
+
+  // player state
+  private bool isJumping = false;
+  // public bool isTouchingWall = false;
+  private bool isGrounded = false;
+  private bool isDashing = false;
+  private float xInput;
+  private float currJumpTime;
+  private float currDashCD;
   public float lastWallJump = 0f;
+  private float currDashDuration;
+  private Vector2 currDirection;
+  public static event Action<float, int> OnDashChange;
 
+  private PlayerInput playerInput;
+  private bool jumpInput;
+  private bool dashInput;
   private Interactable currInteractable;
-
-  private void OnJump(InputAction.CallbackContext context) { isJumpInput = true; }
-  private void OnJumpRelease(InputAction.CallbackContext context) { isJumpInput = false; }
-  private void OnDash(InputAction.CallbackContext context) { isDashingInput = true; }
-  private void OnDashRelease(InputAction.CallbackContext context) { isDashingInput = false; }
-
-  private void SubscribeEvents() {
-    playerInput.actions["Jump"].started += OnJump;
-    playerInput.actions["Jump"].canceled += OnJumpRelease;
-    playerInput.actions["Dash"].started += OnDash;
-    playerInput.actions["Dash"].canceled += OnDashRelease;
-  }
 
   private void Awake() {
     playerInput = GetComponent<PlayerInput>();
     rb = GetComponent<Rigidbody2D>();
     spriteRenderer = GetComponent<SpriteRenderer>();
-    myAnimator = GetComponent<Animator>();
-    SubscribeEvents();
+    animator = GetComponent<Animator>();
   }
 
-  // private void OnCollisionStay2D(Collision2D collider) {
-  //   if (!(collider.gameObject.layer == LayerMask.NameToLayer("Wall"))) return;
-  //   isTouchingWall = true;
-  // }
 
-  // private void OnCollisionExit2D(Collision2D collider) {
-  //   isTouchingWall = false;
-  // }
-
-  // Update is called once per frame
-  void Update() {
-
-    currDirection = spriteRenderer.flipX ? Vector2.left : Vector2.right;
-
-    lastWallJump += Time.deltaTime; // increment the wall jump timer
-    if (lastWallJump < WALL_JUMP_CD) {
-      xInput = 0;
-    } else {
-      xInput = playerInput.actions["Move"].ReadValue<Vector2>().x;
-    }
+  private void PollInput() {
+    jumpInput = playerInput.actions["Jump"].inProgress; // jump input
+    dashInput = playerInput.actions["Dash"].inProgress; // jump input
+    xInput = playerInput.actions["Move"].ReadValue<Vector2>().x; // x input
+    if (lastWallJump < WALL_JUMP_CD) xInput = 0; // (cancel x input)
 
     if (Input.GetButtonDown("Interact")) {
       if (currInteractable) currInteractable.Interact();
     }
 
+  }
+  void Update() {
+    PollInput();
+    // decrement timers
+    currDashCD -= Time.deltaTime;
+    lastWallJump += Time.deltaTime; // increment the wall jump timer
+    currDirection = spriteRenderer.flipX ? Vector2.left : Vector2.right;
+
+
     #region Dash
-    if (isDashingInput && currDashCooldown < 0) {
-      OnDashChange?.Invoke(DASH_COOLDOWN, gameObject.layer);
-      currDashCooldown = DASH_COOLDOWN;
+    if (dashInput && currDashCD < 0) {
+      OnDashChange?.Invoke(DASH_CD, gameObject.layer);
+      currDashCD = DASH_CD;
       isDashing = true;
-      rb.gravityScale = 0;
       currDashDuration = DASH_DURATION;
     }
-    currDashCooldown -= Time.deltaTime;
     #endregion
 
     #region Jump / Ground Check
     // initial jump force
-    if (isJumpInput && isGrounded && !isJumping) {
+    if (jumpInput && isGrounded && !isJumping) {
       rb.AddForce(Vector2.up * JUMP_FORCE, ForceMode2D.Impulse);
       isJumping = true;
-      currJumpTime = MAX_JUMP_TIME;
+      currJumpTime = 0;
     }
 
     isGrounded = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + GROUND_CHECK_OFFSET), Vector2.down, MIN_GROUND_DISTANCE, LayerMask.GetMask("Ground", "Wall"));
     // Debug.DrawRay(new Vector2(transform.position.x, transform.position.y + GROUND_CHECK_OFFSET), Vector2.down * MIN_GROUND_DISTANCE, Color.green);
     #endregion
 
+    #region Wall Jump
+    RaycastHit2D wall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), currDirection, WALL_DETECTOR_LENGTH, LayerMask.GetMask("Wall"));
+    // wall jump
+    if (wall && jumpInput) {
+      rb.velocity = new(wall.normal.x * WALL_JUMP_X_FORCE, WALL_JUMP_Y_FORCE);
+      lastWallJump = 0; /// reset jump timer
+      spriteRenderer.flipX = !spriteRenderer.flipX; // flip the sprite
+    }
+
+    #endregion
     #region Animation / Visuals
-    if (myAnimator == null) return;
-    myAnimator.SetBool("isJump", !isGrounded);
-    myAnimator.SetBool("isWalking", xInput != 0);
-    myAnimator.SetBool("isDash", isDashing);
+    animator.SetBool("isJump", !isGrounded);
+    animator.SetBool("isWalking", xInput != 0);
+    animator.SetBool("isDash", isDashing);
     if (xInput > 0) spriteRenderer.flipX = false;
     if (xInput < 0) spriteRenderer.flipX = true;
     #endregion
@@ -138,25 +121,14 @@ public class PlayerController : MonoBehaviour {
 
   // FixedUpdate is called once every 0.02 seconds or 50 times/second by default
   private void FixedUpdate() {
-    #region Wall Jump
-    RaycastHit2D wall = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), currDirection, WALL_DETECTOR_LENGTH, LayerMask.GetMask("Wall"));
-    isTouchingWall = wall;
-    // Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), currDirection * WALL_DETECTOR_LENGTH, Color.red);
-    // wall jump
-    if (isTouchingWall && isJumpInput) {
-      rb.velocity = new(wall.normal.x * WALL_JUMP_X_FORCE, WALL_JUMP_Y_FORCE);
-      lastWallJump = 0; /// reset jump timer
-      spriteRenderer.flipX = !spriteRenderer.flipX; // flip the sprite
-    }
-    #endregion
     #region Horizontal Movement
 
+    // dash impulse force should be applied in one frame
     if (isDashing) {
       float orientation = spriteRenderer.flipX ? -1 : 1;
       rb.AddForce(orientation * DASH_FORCE * Vector2.right, ForceMode2D.Impulse);
       currDashDuration -= Time.deltaTime;
       if (currDashDuration < 0) {
-        rb.gravityScale = GRAVITY_SCALE;
         isDashing = false;
         currDashDuration = 0;
       }
@@ -170,15 +142,16 @@ public class PlayerController : MonoBehaviour {
     #endregion
 
     // continuously add force each frame while jump key is held down
-    if (isJumpInput && currJumpTime > 0f && isJumping) {
+    if (jumpInput && currJumpTime < MAX_JUMP_TIME && isJumping) {
       rb.AddForce(Vector2.up * JUMP_FORCE);
-      currJumpTime -= Time.deltaTime;
+      currJumpTime += Time.deltaTime;
     } else {
       isJumping = false;
-    }
 
-    rb.gravityScale = rb.velocity.y < 0 ? GRAVITY_SCALE * GRAVITY_MULTIPLIER : GRAVITY_SCALE;
-    if (rb.velocity.y < -MAX_FALL_SPEED) rb.velocity = new Vector2(rb.velocity.x, -MAX_FALL_SPEED);
+
+      // clamp fall speed
+      if (rb.velocity.y < -MAX_FALL_SPEED) rb.velocity = new Vector2(rb.velocity.x, -MAX_FALL_SPEED);
+    }
   }
 
   public void SetInteractable(Interactable interactable) {
